2f7d22089787951c90d6b68de7be932e2c668152
张超 <zhangchao@qie.tv>
2019-06-17 21:30:04 +0800

    --

---
+++ app/Admin/routes.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Routing\Router;
+
+Admin::routes();
+
+Route::group([
+    'prefix'        => config('admin.route.prefix'),
+    'namespace'     => config('admin.route.namespace'),
+    'middleware'    => config('admin.route.middleware'),
+], function (Router $router) {
+
+    $router->get('/', 'HomeController@index')->name('admin.home');
+
+    // 员工管理
+
+    // 分公司管理
+
+    // 业绩管理
+
+    // 
+
+});
